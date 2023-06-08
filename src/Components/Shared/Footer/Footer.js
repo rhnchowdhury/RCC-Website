7203550e7a5818356948a95afb72c7414f97f905
Raihan <rhnchdri@gmail.com>
2023-06-08 18:11:18 +0600

    footer added

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-
+        <div className='bg-gray-500'>
+            <div className='text-center'>
+                <h1 className='text-4xl font-medium text-white'>CONTACT US</h1>
+                <p className='text-lg text-white'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus nihil facilis reiciendis officiis <br /> consectetur reprehenderit eveniet, blanditiis illo exercitationem possimus?</p>
+            </div>
+            <div className='ml-32 space-y-5 my-5 place-items-center'>
+                <div className='space-x-5'>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-lg" />
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                </div>
+                <div>
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                </div>
+                <div>
+                    <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Shared/NavBar/NavBar.js
+++ src/Components/Shared/NavBar/NavBar.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div>
-
+        <div className="navbar">
+            <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                </label>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Services</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
