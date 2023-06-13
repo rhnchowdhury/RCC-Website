2e62f6e93ba337319e35845bc7d6ca70639df4a9
Raihan <rhnchdri@gmail.com>
2023-06-13 18:50:48 +0600

    add some part

--- src/Components/Pages/Abouts/About.js
+++ src/Components/Pages/Abouts/About.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-
-        </div>
+        <section>
+            <h1 className='font-bold text-4xl'>ABOUT US</h1>
+            <div className='flex'>
+                <div>
+                    <h3>After being one of the most dynamic digital agencies in Bangladesh, RainCloud has expanded its wings and evolved into a fully-fledged 360 Marketing agency. Our mission is to deliver performance marketing objectives. Our expert teams are dedicated to advance your Brand’s performance, from strategy to execution. The dynamic team is a growing mashup of creative, technical, marketing & sales people. Over the years we have managed numerous brands both International and local, which gave the team priceless insights, knowledge and experience.</h3>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero neque tenetur, accusamus rerum cumque, praesentium eum vel minus obcaecati, eos velit perspiciatis omnis quas nemo perferendis quibusdam ducimus? Optio ipsam fugit natus facilis fugiat provident aut tempore exercitationem iusto quasi? Perferendis neque expedita amet iure eos nobis consectetur quisquam voluptates, aspernatur tenetur suscipit saepe odit beatae culpa animi placeat.</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
