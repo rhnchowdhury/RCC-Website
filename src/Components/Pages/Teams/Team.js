e34bf03a9e1b613077dc9cb82f8e54ab44347e39
Raihan <rhnchdri@gmail.com>
2023-06-13 17:00:41 +0600

    img added in team part

--- src/Components/Pages/Teams/Team.js
+++ src/Components/Pages/Teams/Team.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import img1 from '../../../teams/Azfar-Rahman.png';
+import img2 from '../../../teams/Fahmeda-Gulzar.png';
+import img3 from '../../../teams/Mir-Ibtid-Mahmud.png';
+import img4 from '../../../teams/Sadman-al-sadaf.png';
 
 const Team = () => {
     return (
@@ -7,6 +11,14 @@ const Team = () => {
             <div className='text-center'>
                 <h1 className=" text-4xl font-medium">Our Team</h1>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio cumque <br /> tempora hic optio quasi assumenda.</p>
+            </div>
+            <div>
+                <div className='grid grid-cols-4 gap-4'>
+                    <img src={img1} alt="" />
+                    <img src={img2} alt="" />
+                    <img src={img3} alt="" />
+                    <img src={img4} alt="" />
+                </div>
             </div>
         </section>
     );
