8a6b29aebc68d8e9870d3065bf392be7a9db3631
Raihan <rhnchdri@gmail.com>
2023-06-13 16:02:45 +0600

    team create

--- src/Components/Pages/Homes/Home.js
+++ src/Components/Pages/Homes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Service from '../Services/Service';
 import Message from '../Messages/Message';
+import Team from '../Teams/Team';
 
 const Home = () => {
     return (
         <div>
+            <Team></Team>
             <Service></Service>
             <Message></Message>
         </div>
---
+++ src/Components/Pages/Teams/Team.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Team = () => {
+    return (
+        <section>
+            <p className='text-center'>CREATIVE MINDS</p>
+            <div className='text-center'>
+                <h1 className=" text-4xl font-medium">Our Team</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio cumque <br /> tempora hic optio quasi assumenda.</p>
+            </div>
+        </section>
+    );
+};
+
+export default Team;
