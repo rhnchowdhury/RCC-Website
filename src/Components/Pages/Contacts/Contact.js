5661086d37ca0b730e2e3b7d61046055cafc35c3
Raihan <rhnchdri@gmail.com>
2023-06-13 14:28:29 +0600

    contact done

--- src/Components/Pages/Contacts/Contact.js
+++ src/Components/Pages/Contacts/Contact.js
@@ -7,31 +7,31 @@ import Map from './Map';
 const Contact = () => {
     return (
         <section className='my-10'>
-            <div className='flex'>
+            <div className='flex mx-12'>
                 <div>
                     <div className='flex'>
                         <span className=''><FcCellPhone className='w-8 h-12'></FcCellPhone></span>
-                        <div>
+                        <div className='ml-2'>
                             <p>PHONE</p>
                             <p>+8801777750875</p>
                         </div>
                     </div>
                     <div className='flex mt-8'>
                         <AiOutlineMail className='w-8 h-12'></AiOutlineMail>
-                        <div>
+                        <div className='ml-2'>
                             <p>EMAIL:</p>
                             <p>raincloud.hr@gmail.com</p>
                         </div>
                     </div>
                     <div className='flex mt-8'>
                         <RiUserLocationLine className='w-8 h-12'></RiUserLocationLine>
-                        <div>
+                        <div className='ml-2'>
                             <p>OFFICE:</p>
                             <p>Rangs Shawpnil, Level 5, House 2, Road ⅔, Banani, Dhaka 1213</p>
                         </div>
                     </div>
                 </div>
-                <div className=''>
+                <div>
                     <div className='grid grid-cols-2 gap-5 ml-56'>
                         <input type="text" placeholder="Your Name" className="input input-bordered w-full max-w-xs" />
                         <input type="text" placeholder="Email Address" className="input input-bordered w-full max-w-xs" />
