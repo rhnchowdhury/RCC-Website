8c8c9add1071bb6d215350a19238d49a0cca0673
Raihan <rhnchdri@gmail.com>
2023-06-14 16:07:38 +0600

    work part added

--- src/Components/Pages/Homes/Home.js
+++ src/Components/Pages/Homes/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Service from '../Services/Service';
 import Message from '../Messages/Message';
 import Team from '../Teams/Team';
+import Work from '../Works/Work';
 
 const Home = () => {
     return (
         <div>
+            <Work></Work>
             <Team></Team>
             <Service></Service>
             <Message></Message>
---
+++ src/Components/Pages/Works/Work.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Work = () => {
+    return (
+        <section>
+            <p className='text-center'>WE ARE AWESOME AGENCY</p>
+            <div className='text-center'>
+                <h1 className=" text-4xl font-medium">WHAT WE DO</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Work;
