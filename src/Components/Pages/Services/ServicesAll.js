d60117e6207c23b0c0d0025af5ad9d82d9dcc2d9
Raihan <rhnchdri@gmail.com>
2023-06-14 15:06:55 +0600

    ecosystem add

--- src/Components/Pages/Services/ServicesAll.js
+++ src/Components/Pages/Services/ServicesAll.js
@@ -18,7 +18,7 @@ const ServicesAll = () => {
                     </div>
                 </div>
             </div>
-            <div className="hero">
+            <div className="hero my-12">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={img1} alt='' className="max-w-md rounded-lg shadow-2xl" />
                     <div className='ml-12'>
@@ -27,6 +27,22 @@ const ServicesAll = () => {
                             <br /> <span className='font-bold'>1. Page Moderation:</span>Monitoring and controlling the content that is posted on a webpage or social media platform. It involves reviewing and managing user-generated content, such as comments, messages, and posts, to ensure that they comply with the platform's guidelines and community standards.
                             <br /> <span className='font-bold'>2. Creative Content:</span> Unleashing the power of imagination and using it to create something original and engaging that resonates with people on a deeper level.
                             <br /> <span className='font-bold' >3. Query management:</span>Handling and organising queries or requests from various sources, such as customers, clients, or users. It involves capturing, categorising, prioritising, and resolving queries efficiently and effectively.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={img1} alt='' className="max-w-md rounded-lg shadow-2xl" />
+                    <div className='ml-12'>
+                        <h1 className="text-5xl font-bold">Digital Ecosystem:</h1>
+                        <p className="mt-12">Digital ecosystems can help your business to grow on digital advertising platforms with an interconnected and integrated system consisting of brand guidelines, improving customer experiences, and gathering customer behaviour data, website traffic data, lead conversion, and the effectiveness of your online promotion.
+                            <br /> <span className='font-bold'>1. Brand Guidelines</span>
+                            <br /> <span className='font-bold'>2. Google, Facebook. Instagram, Linkedin, YouTube ads</span>
+                            <br /> <span className='font-bold'>3. Facebook pixel</span>
+                            <br /> <span className='font-bold'>4. Google Analytics Integration</span>
+                            <br /> <span className='font-bold'>5. Facebook and Instagram Shop set up </span>
+                            <br /> <span className='font-bold'>6. Website Development</span>
                         </p>
                     </div>
                 </div>
