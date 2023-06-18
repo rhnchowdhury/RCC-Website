f77f17aa2d8e95a0b6d00ce814d04743bb85f185
Raihan <rhnchdri@gmail.com>
2023-06-18 18:00:47 +0600

    sidebar added

--- src/Components/Shared/NavBar/NavBar.js
+++ src/Components/Shared/NavBar/NavBar.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AdjustmentsVerticalIcon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className="navbar">
-            <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/team'>Our Teams</Link></li>
-                    <li><Link to='/service'>Services</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
-                </ul>
+
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div onClick={() => setOpen(!open)} className="h-6 w-6 text-blue-500">
+                            {
+                                open ?
+                                    <XMarkIcon />
+                                    :
+                                    <AdjustmentsVerticalIcon />
+                            }
+
+
+                        </div>
+                    </label>
+                    <ul className={`mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ${!open ? 'right-32' : 'right-[-32]'}`}>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/about'>About</Link></li>
+                        <li><Link to='/team'>Our Teams</Link></li>
+                        <li><Link to='/service'>Services</Link></li>
+                        <li><Link to='/contact'>Contact</Link></li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
