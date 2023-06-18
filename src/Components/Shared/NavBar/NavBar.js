415d92268610600eace324dc7909e8a374705ab3
Raihan <rhnchdri@gmail.com>
2023-06-18 18:25:58 +0600

    logo add

--- src/Components/Shared/NavBar/NavBar.js
+++ src/Components/Shared/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AdjustmentsVerticalIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import logo from '../../../Icon/Rcc logo.png';
 
 const NavBar = () => {
     const [open, setOpen] = useState(false);
@@ -8,7 +9,8 @@ const NavBar = () => {
 
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
+                <Link to='/'><img src={logo} alt="RainCloud" className='w-16 h-20' /></Link>
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
@@ -27,7 +29,7 @@ const NavBar = () => {
 
                         </div>
                     </label>
-                    <ul className={`mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ${!open ? 'right-32' : 'right-[-32]'}`}>
+                    <ul className={`mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ${open ? 'right-32' : 'right-[-32]'}`}>
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='/about'>About</Link></li>
                         <li><Link to='/team'>Our Teams</Link></li>
