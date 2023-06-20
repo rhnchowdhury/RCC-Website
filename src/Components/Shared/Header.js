f3b3aff9da112e438a45a48de0a8186329495331
Raihan <rhnchdri@gmail.com>
2023-06-20 12:23:51 +0600

    video added

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NavBar/Header.css';
+import reel from '../../../src/Reel/All Scene_4.mp4';
 
 const Header = () => {
     return (
@@ -8,8 +9,9 @@ const Header = () => {
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque assumenda exercitationem doloremque deserunt accusamus cum eveniet sit fugiat officia iure!</p>
             </div>
             <div className='para-2'>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At fugiat veniam consectetur a enim reprehenderit alias temporibus ab porro totam.</p>
+                {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At fugiat veniam consectetur a enim reprehenderit alias temporibus ab porro totam.</p> */}
             </div>
+            <video src={reel}></video>
         </div>
     );
 };
