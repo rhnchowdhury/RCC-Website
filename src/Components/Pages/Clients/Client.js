f215b242baa0e359b24c8ee99bc1f3e4c32a61f0
Raihan <rhnchdri@gmail.com>
2023-07-06 18:38:53 +0600

    edited some portioon

--- src/Components/Pages/Clients/Client.js
+++ src/Components/Pages/Clients/Client.js
@@ -31,7 +31,8 @@ import img29 from '../../../Clients/total care.png';
 
 const Client = () => {
     return (
-        <section>
+        <section className='my-5'>
+            <h1 className="text-center text-4xl font-medium">OUR CLIENT</h1>
             {/* <div className='grid grid-cols-5 gap-4 bg-zinc-200'>
                 <img src={img1} alt='' className="w-40" />
                 <img src={img2} alt='' className="w-40" />
@@ -64,58 +65,68 @@ const Client = () => {
                 <img src={img29} alt='' className="w-40" />
             </div> */}
 
-            <div className="carousel w-full h-60">
-                <div id="item1" className="carousel-item w-full">
-                    <img src={img1} alt='' className="w-60 h-32" />
-                    <img src={img2} alt='' className="w-60 h-32" />
-                    <img src={img3} alt='' className="w-60 h-32" />
-                    <img src={img4} alt='' className="w-60 h-32" />
-                    <img src={img5} alt='' className="w-60 h-32" />
+            <div className='flex mx-8 mt-8'>
+                <div className='W-1/2 h-1/2'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis totam iure. Consequuntur odio cum magnam quidem laboriosam. Distinctio rem quaerat odio quod, quas cum. Magnam eos ipsum, cum dolor aperiam molestias et consequuntur reprehenderit ipsam, saepe voluptatibus. Nulla quae facere quidem, architecto officiis voluptatem iste. Voluptas veniam perspiciatis nostrum unde expedita rerum temporibus error autem iure perferendis tenetur a ducimus ipsum adipisci similique animi, iste, neque accusantium alias eligendi repellat quisquam. Hic accusamus ipsum eos eum adipisci iusto dolorem ex molestiae, animi voluptatibus, ducimus impedit. Quo commodi ab quasi tenetur molestiae odit, corrupti recusandae nesciunt blanditiis, cum aperiam eius aliquam nisi repellat porro nam animi modi ex sapiente, perspiciatis aut rerum! Laborum ex harum recusandae eum facilis saepe earum blanditiis omnis delectus excepturi doloremque animi, at placeat quisquam vitae in voluptas, explicabo ad, assumenda nam et accusamus error! Labore assumenda nulla cum, qui amet praesentium ipsa dicta nam ipsum.</p>
+                </div>
 
+                <div className='W-1/2 h-1/2 ml-24'>
+                    <div className="carousel w-full h-40">
+                        <div id="item1" className="carousel-item w-full">
+                            <img src={img1} alt='' className="w-24 h-16" />
+                            <img src={img2} alt='' className="w-24 h-16" />
+                            <img src={img3} alt='' className="w-24 h-16" />
+                            <img src={img4} alt='' className="w-24 h-16" />
+                            <img src={img5} alt='' className="w-24 h-16" />
+                            {/* <img src={img4} alt='' className="w-60 h-32" />
+                            <img src={img5} alt='' className="w-60 h-32" /> */}
+
+                        </div>
+                        <div id="item2" className="carousel-item w-full">
+                            <img src={img6} alt='' className="w-60 h-32" />
+                            <img src={img7} alt='' className="w-60 h-32" />
+                            <img src={img8} alt='' className="w-60 h-32" />
+                            <img src={img9} alt='' className="w-60 h-32" />
+                            <img src={img10} alt='' className="w-60 h-32" />
+                        </div>
+                        <div id="item3" className="carousel-item w-full">
+                            <img src={img11} alt='' className="w-60 h-32" />
+                            <img src={img12} alt='' className="w-60 h-32" />
+                            <img src={img13} alt='' className="w-60 h-32" />
+                            <img src={img14} alt='' className="w-60 h-32" />
+                            <img src={img15} alt='' className="w-60 h-32" />
+                        </div>
+                        <div id="item4" className="carousel-item w-full">
+                            <img src={img16} alt='' className="w-60 h-32" />
+                            <img src={img17} alt='' className="w-60 h-32" />
+                            <img src={img18} alt='' className="w-60 h-32" />
+                            <img src={img19} alt='' className="w-60 h-32" />
+                            <img src={img20} alt='' className="w-60 h-32" />
+                        </div>
+                        <div id="item5" className="carousel-item w-full">
+                            <img src={img21} alt='' className="w-60 h-32" />
+                            <img src={img22} alt='' className="w-60 h-32" />
+                            <img src={img23} alt='' className="w-60 h-32" />
+                            <img src={img24} alt='' className="w-60 h-32" />
+                            <img src={img25} alt='' className="w-60 h-32" />
+                        </div>
+                        <div id="item6" className="carousel-item w-full">
+                            <img src={img26} alt='' className="w-60 h-32" />
+                            <img src={img27} alt='' className="w-60 h-32" />
+                            <img src={img28} alt='' className="w-60 h-32" />
+                            <img src={img29} alt='' className="w-60 h-32" />
+                            <img src={img20} alt='' className="w-60 h-32" />
+                        </div>
+                    </div>
+                    <div className="flex justify-center w-full py-2 gap-2">
+                        <a href="#item1" className="btn btn-xs">1</a>
+                        <a href="#item2" className="btn btn-xs">2</a>
+                        <a href="#item3" className="btn btn-xs">3</a>
+                        <a href="#item4" className="btn btn-xs">4</a>
+                        <a href="#item5" className="btn btn-xs">5</a>
+                        <a href="#item6" className="btn btn-xs">6</a>
+                    </div>
                 </div>
-                <div id="item2" className="carousel-item w-full">
-                    <img src={img6} alt='' className="w-60 h-32" />
-                    <img src={img7} alt='' className="w-60 h-32" />
-                    <img src={img8} alt='' className="w-60 h-32" />
-                    <img src={img9} alt='' className="w-60 h-32" />
-                    <img src={img10} alt='' className="w-60 h-32" />
-                </div>
-                <div id="item3" className="carousel-item w-full">
-                    <img src={img11} alt='' className="w-60 h-32" />
-                    <img src={img12} alt='' className="w-60 h-32" />
-                    <img src={img13} alt='' className="w-60 h-32" />
-                    <img src={img14} alt='' className="w-60 h-32" />
-                    <img src={img15} alt='' className="w-60 h-32" />
-                </div>
-                <div id="item4" className="carousel-item w-full">
-                    <img src={img16} alt='' className="w-60 h-32" />
-                    <img src={img17} alt='' className="w-60 h-32" />
-                    <img src={img18} alt='' className="w-60 h-32" />
-                    <img src={img19} alt='' className="w-60 h-32" />
-                    <img src={img20} alt='' className="w-60 h-32" />
-                </div>
-                <div id="item5" className="carousel-item w-full">
-                    <img src={img21} alt='' className="w-60 h-32" />
-                    <img src={img22} alt='' className="w-60 h-32" />
-                    <img src={img23} alt='' className="w-60 h-32" />
-                    <img src={img24} alt='' className="w-60 h-32" />
-                    <img src={img25} alt='' className="w-60 h-32" />
-                </div>
-                <div id="item6" className="carousel-item w-full">
-                    <img src={img26} alt='' className="w-60 h-32" />
-                    <img src={img27} alt='' className="w-60 h-32" />
-                    <img src={img28} alt='' className="w-60 h-32" />
-                    <img src={img29} alt='' className="w-60 h-32" />
-                    <img src={img20} alt='' className="w-60 h-32" />
-                </div>
-            </div>
-            <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-                <a href="#item3" className="btn btn-xs">3</a>
-                <a href="#item4" className="btn btn-xs">4</a>
-                <a href="#item5" className="btn btn-xs">5</a>
-                <a href="#item6" className="btn btn-xs">6</a>
             </div>
         </section >
     );
