6c5041d6db20ddb84d2b1d4cabd684667550bb23
Raihan <rhnchdri@gmail.com>
2023-07-26 18:44:45 +0600

    edit on team img

--- src/Components/Pages/Teams/Team.js
+++ src/Components/Pages/Teams/Team.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsLinkedin } from "react-icons/bs";
 import img1 from '../../../teams/Azfar-Rahman.png';
 import img2 from '../../../teams/Fahmeda-Gulzar.png';
 import img3 from '../../../teams/Golam-Isnad-Ahmed.png';
@@ -12,6 +13,8 @@ import img10 from '../../../teams/Redwanur-Rahman-Rafi.png';
 import img11 from '../../../teams/Sadia-Hossain-Lubna.png';
 import img12 from '../../../teams/Sadman-al-sadaf.png';
 import img13 from '../../../teams/Sharif-Hossen.png';
+import './Team.css';
+import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
 const Team = () => {
@@ -23,23 +26,18 @@ const Team = () => {
                 {/* <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio cumque <br /> tempora hic optio quasi assumenda.</p> */}
                 <p className='mt-4'>We pride ourselves on being a dynamic and innovative group of professionals dedicated <br /> to delivering exceptional results for our clients.</p>
             </div>
-            {/* <div>
-                <div className='grid grid-cols-4 gap-4'>
-                    <img src={img1} alt="" />
-                    <img src={img2} alt="" />
-                    <img src={img3} alt="" />
-                    <img src={img4} alt="" />
-                </div>
-                <div className='flex justify-center my-5'>
-                    <button className="btn btn-outline"><Link to='/team'>See All</Link></button>
-                </div>
-            </div> */}
-
 
             <div className="carousel w-full mt-5">
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src={img1} alt='' className="w-96" />
-                    <img src={img2} alt='' className="w-96 ml-2" />
+                    <div className='img-edit'>
+                        <img src={img2} alt='' className="w-96 ml-2" />
+                        <div className='overlay'>
+                            <Link className=''><BsLinkedin className=' logo social-icon'></BsLinkedin></Link>
+                            {/* <span className=' w-8 h-12'>
+                            </span> */}
+                        </div>
+                    </div>
                     <img src={img3} alt='' className="w-96 ml-2" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-10 top-1/2">
                         <a href="#slide5" className="btn btn-circle">❮</a>
