d1d29c44744a36ee9cdf0098018747823deecb67
Raihan <rhnchdri@gmail.com>
2023-07-31 12:45:49 +0600

    another team img hover

--- src/Components/Pages/Teams/Team.js
+++ src/Components/Pages/Teams/Team.js
@@ -73,6 +73,26 @@ const Team = () => {
                         </div>
                     </div>
                     <img src={img5} alt='' className="w-96" />
+                    <img src={img8} alt='' className="w-96 ml-2" />
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-10 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <div className='img-edit'>
+                        <img src={img12} alt='' className="w-96 ml-2" />
+                        <div className='overlay'>
+                            <div>
+                                <Link to='https://www.linkedin.com/in/sadman-al-sadaf-7b700a110/' target="_blank"><BsLinkedin className='social-icon'></BsLinkedin></Link>
+                                <Link to='https://mail.google.com/sadman@raincloudcomms.com' target='_blank'><SiGmail className=' social-icon '></SiGmail></Link>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={img7} alt='' className="w-96" />
+
+
                     <div className='img-edit'>
                         <img src={img6} alt='' className="w-96" />
                         <div className='overlay'>
@@ -83,15 +103,6 @@ const Team = () => {
                         </div>
                     </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-10 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src={img7} alt='' className="w-96" />
-                    <img src={img8} alt='' className="w-96 ml-2" />
-                    <img src={img9} alt='' className="w-96 ml-2" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-10 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
                     </div>
@@ -99,7 +110,7 @@ const Team = () => {
                 <div id="slide4" className="carousel-item relative w-full">
                     <img src={img10} alt='' className="w-96" />
                     <img src={img11} alt='' className="w-96 ml-2" />
-                    <img src={img12} alt='' className="w-96 ml-2" />
+                    <img src={img9} alt='' className="w-96 ml-2" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-10 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide5" className="btn btn-circle">❯</a>
