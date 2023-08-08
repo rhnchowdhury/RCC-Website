3ef584783343942709a23d11e87040824219d9ac
Raihan <rhnchdri@gmail.com>
2023-08-08 15:54:24 +0600

    some img add

--- src/Components/Pages/Works/Work.js
+++ src/Components/Pages/Works/Work.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import img1 from '../../../Client work/Eden Apple Pie 12.09.22.jpg';
+import img2 from '../../../Client work/Gazi Pipe 24.10.22 v2.jpg';
+import img3 from '../../../Client work/Pipe_Post_25_26december2022.png';
+import img4 from '../../../Client work/Post 1.png';
+import img5 from '../../../Client work/Post-08_05-09-2022.png';
+import img6 from '../../../Client work/Post-61_16-10-2022.png';
+import img7 from '../../../Client work/Salmon Salad 14.09.22 v2.jpg';
+import img8 from '../../../Client work/Shades-13-03-22-v2.png';
 
 const Work = () => {
     return (
@@ -8,56 +16,40 @@ const Work = () => {
                 <h1 className=" text-4xl font-medium">OUR WORK</h1>
                 <p className='text-lg mt-4'>We are a dedicated team committed to providing you with exceptional <br /> solutions and top-notch services.</p>
             </div>
-            <div class="container lg:px-16 px-4 py-8 mx-auto items-center ">
+            <div class="container lg:mx-16  mx-auto items-center ">
                 <div class="grid grid-cols-4 grid-rows-4 grid-flow-col gap-2">
-                    <div class="w-full row-span-2">
+                    <div class="w-full h-96 row-span-2">
                         <img
-                            src="https://images.unsplash.com/photo-1432457990754-c8b5f21448de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100" />
+                            src={img1} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100" />
                     </div>
                     <div class="w-full col-span-2 row-span-2">
                         <img
-                            src="https://images.unsplash.com/photo-1586921829167-409624a3734a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
+                            src={img2} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
                     </div>
                     <div class="w-full ">
                         <img
-                            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
+                            src={img3} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
                     </div>
                     <div class="w-full">
                         <img
-                            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
+                            src={img4} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
                     </div>
                     <div class="w-full col-span-2 row-span-2">
                         <img
-                            src="https://images.unsplash.com/photo-1586921829167-409624a3734a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
+                            src={img5} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
                     </div>
 
                     <div class="w-full col-span-2">
                         <img
-                            src="https://images.unsplash.com/photo-1534531173927-aeb928d54385?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
+                            src={img6} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
                     </div>
                     <div class="w-full">
                         <img
-                            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
+                            src={img7} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
                     </div>
                     <div class="w-full">
                         <img
-                            src="https://images.unsplash.com/photo-1489981424607-45c58daf0581?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                            alt=""
-                            class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
+                            src={img8} alt="" class="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 " />
                     </div>
                 </div>
             </div>
