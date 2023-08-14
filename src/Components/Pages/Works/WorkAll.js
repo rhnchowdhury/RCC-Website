73037b9fa9e7fe31ba39ea3ab121173a4d630616
Raihan <rhnchdri@gmail.com>
2023-08-14 17:43:53 +0600

    some img added in galary section

--- src/Components/Pages/Works/WorkAll.js
+++ src/Components/Pages/Works/WorkAll.js
@@ -11,6 +11,18 @@ import img9 from '../../../Client work/SLI - May 8.jpg';
 import img10 from '../../../Client work/SH - July 31.png';
 import img11 from '../../../Client work/Dekchi - July 30.png';
 import img12 from '../../../Client work/Eden Apple Pie 12.09.22.jpg';
+import img14 from '../../../Client work/Post-61_16-10-2022.png';
+import img13 from '../../../Client work/Chilis - July 22.jpg';
+import img15 from '../../../Client work/L-H_Post12_12june2023.png';
+import img16 from '../../../Client work/SH - June 21.png';
+import img17 from '../../../Client work/Santoor - June 10.png';
+import img18 from '../../../Client work/Door_post25_04january2023.png';
+import img19 from '../../../Client work/Chilis - July 5.jpg';
+import img20 from '../../../Client work/Post-3.png';
+import img21 from '../../../Client work/L-H_Post22_10july2023.png';
+import img22 from '../../../Client work/Post 6.png';
+import img23 from '../../../Client work/SFD - July 13.jpg';
+import img24 from '../../../Client work/Post-26_25-09-2022-GaziDoor_Rumi.png';
 
 const WorkAll = () => {
     return (
@@ -51,6 +63,42 @@ const WorkAll = () => {
                 </div>
                 <div>
                     <img src={img12} alt="" className='' />
+                </div>
+                <div>
+                    <img src={img13} alt="" className='h-64' />
+                </div>
+                <div>
+                    <img src={img14} alt="" className='' />
+                </div>
+                <div>
+                    <img src={img15} alt="" className='h-64' />
+                </div>
+                <div>
+                    <img src={img16} alt="" className='' />
+                </div>
+                <div>
+                    <img src={img17} alt="" className='h-64' />
+                </div>
+                <div>
+                    <img src={img18} alt="" className='' />
+                </div>
+                <div>
+                    <img src={img19} alt="" className='' />
+                </div>
+                <div>
+                    <img src={img20} alt="" className='-mt-14 h-64' />
+                </div>
+                <div>
+                    <img src={img21} alt="" className='' />
+                </div>
+                <div>
+                    <img src={img22} alt="" className='-mt-14 h-64' />
+                </div>
+                <div>
+                    <img src={img23} alt="" className='' />
+                </div>
+                <div>
+                    <img src={img24} alt="" className='-mt-14 h-64' />
                 </div>
 
 
